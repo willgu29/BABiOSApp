819752cc1357aa024216689a0bf96f31e3e6e458
Will Gu <willgu29@gmail.com>
2015-08-07 15:03:29 -0700

    First commit

---
+++ Bruin App BuildersTests/Bruin_App_BuildersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bruin_App_BuildersTests.swift
+//  Bruin App BuildersTests
+//
+//  Created by William Gu on 8/7/15.
+//  Copyright (c) 2015 Gu Studios. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Bruin_App_BuildersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
