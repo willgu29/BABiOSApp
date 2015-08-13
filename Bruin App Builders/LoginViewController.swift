f6ab29c1624398ccf7ad8eacdfbc5d83ed5a6f32
Suchit Panjiyar <suchitpanjiyar@Suchits-MBP.limitless>
2015-08-13 15:01:22 -0700

    Added constraints to username and password

--- Bruin App Builders/LoginViewController.swift
+++ Bruin App Builders/LoginViewController.swift
@@ -12,9 +12,7 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
-    
-    
-    
+    @IBOutlet weak var errorMessageLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -28,11 +26,60 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func isUserNameWrong(un : String) -> Bool
+    {
+        var twoParts = split(un) {$0 == "@"}
+        if (twoParts.count != 2)
+        {
+            return true
+        }
+        var dotChecker = split(twoParts[1]) {$0 == "."}
+        if dotChecker.count <= 1
+        {
+            return true
+        }
+        return false
+    }
+    
+    func isPasswordWrong(pw : String) -> Bool
+    {
+        if (count(pw) < 6)
+        {
+            return true
+        }
+        return false
+    }
+    
     @IBAction func goToMainViewController(sender: UIButton)
     {
-        //Authentication
-        
-        presentViewController(MainPageViewController(), animated: true, completion: nil)
+        if (usernameText.text == "")
+        {
+            if (passwordText.text == "")
+            {
+                errorMessageLabel.text = "Please enter a username and password"
+            }
+            else
+            {
+                errorMessageLabel.text = "Please enter a username"
+            }
+        }
+        else if (passwordText.text == "")
+        {
+            errorMessageLabel.text = "Please enter a password"
+        }
+        else if isUserNameWrong(usernameText.text)
+        {
+            errorMessageLabel.text = "Please enter a valid username"
+        }
+        else if isPasswordWrong(passwordText.text)
+        {
+            errorMessageLabel.text = "Please enter a valid password"
+        }
+        else
+        {
+            //Authentication
+            presentViewController(MainPageViewController(), animated: true, completion: nil)
+        }
     }
     
     
