4c27952babdd7901bd7901f291557533fa0d2024
Will Gu <willgu29@gmail.com>
2015-08-21 17:38:52 -0700

    Added create account and get users api

--- Bruin App Builders/APIURLs.h
+++ Bruin App Builders/APIURLs.h
@@ -9,8 +9,30 @@
 #ifndef Bruin_App_Builders_APIURLs_h
 #define Bruin_App_Builders_APIURLs_h
 
+//replace with www.igrouply.com in production (note www. so it doesn't route)
+//pass in variables are CASE-SENSITIVE, pass in JSON format.
+// :variableName denotes the string you should append onto the call
 
-#define API_POST_LOGIN @"http://www.igrouply.com/login"  //pass in email : ..,  password: ..
 
+//Login user
+#define API_POST_LOGIN @"https://gsm.herokuapp.com/login"  //pass in email : ..,  password: ..
+
+//Create account
+#define API_POST_CREATE_ACCOUNT @"https://gsm.herokuapp.com/createAccount" //pass in email, password,  phoneNumber, firstName, lastName
+
+//Requires logged in user to access endpoints
+
+
+    //Get all users
+#define API_GET_USERS @"https://gsm.herokuapp.com/api/users" //add "/:email" to get specific user
+
+
+
+
+
+
+
+
+//***********
 
 #endif
